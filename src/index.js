62fa3c54faddb65db0b71d88d8956029cb2476c7
jia <>
2022-07-17 11:16:54 +0800

    0717--

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import GlobalStyles from "./styles";
 import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-
+//
 const client = new ApolloClient({
   uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
   cache: new InMemoryCache(),
